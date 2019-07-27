4495cafaa55d975f0fd687d458e5a4d4d2f814d3
Nicholas Ignoffo <nick@tehnut.info>
2019-07-26 20:35:06 -0700

    Add chapters, episodes, and next episode to footer

--- index.js
+++ index.js
@@ -84,10 +84,68 @@ function handleMedia(type, contents, channel) {
       .setAuthor(media.title.romaji, "https://anilist.co/img/logo_al.png", media.url)
       .setDescription(sanitizeDescription(media.description))
       .setThumbnail(media.image.extraLarge)
-      .setColor(media.image.color || 4044018);
+      .setColor(media.image.color || 4044018)
+      .setFooter(getFooterText(media));
 
     channel.send(embed)
   });
+
+  function getFooterText(media) {
+    switch (type) {
+      case "MANGA": {
+        if (media.chapters)
+          return `${media.chapters} Chapter(s)`;
+
+        return "";
+      }
+      case "ANIME": {
+        let ret = "";
+        if (media.episodes)
+          ret += `${media.episodes} Episode(s)`;
+
+        if (media.airingSchedule && media.airingSchedule.nodes.length > 0) {
+          const next = media.airingSchedule.nodes[0];
+          ret += `${ret.length > 0 ? " | " : ""} ${formatTime(next.timeUntilAiring)} until episode ${next.episode}`;
+        }
+        return ret;
+      }
+      default: return "";
+    }
+  }
+}
+
+function parseTime(secs) {
+  let seconds = parseInt(secs, 10);
+
+  let weeks = Math.floor(seconds / (3600 * 24 * 7));
+  seconds -= weeks * 3600 * 24 * 7;
+  let days = Math.floor(seconds / (3600 * 24));
+  seconds -= days * 3600 * 24;
+  let hours = Math.floor(seconds / 3600);
+  seconds -= hours * 3600;
+  let minutes = Math.floor(seconds / 60);
+  seconds -= minutes * 60;
+
+  return { weeks, days, hours, minutes, seconds };
+}
+
+function formatTime(secs, appendSeconds) {
+  const time = parseTime(secs);
+
+  let ret = "";
+  if (time.weeks > 0)
+    ret += time.weeks + "w";
+  if (time.days > 0)
+    ret += (ret.length === 0 ? "" : " ") + time.days + "d";
+  if (time.hours > 0)
+    ret += (ret.length === 0 ? "" : " ") + time.hours + "h";
+  if (time.minutes > 0)
+    ret += (ret.length === 0 ? "" : " ") + time.minutes + "m";
+
+  if (appendSeconds && time.seconds > 0)
+    ret += (ret.length === 0 ? "" : " ") + time.seconds + "s";
+
+  return ret;
 }
 
 const url = "https://graphql.anilist.co/";
